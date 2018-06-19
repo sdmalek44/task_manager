67f3e52a6cd44e778281796d33c17854484e89b9
Stephen Malek <malek.stephen44@gmail.com>
2018-06-18 19:07:35 -0600

    allow user to edit and update tasks

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -2,6 +2,7 @@ require_relative '../models/task.rb'
 
 class TaskManagerApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
+  set :method_override, true
 
   get '/' do
     erb :dashboard
@@ -20,5 +21,21 @@ class TaskManagerApp < Sinatra::Base
     task = Task.new(params[:task])
     task.save
     redirect '/tasks'
+  end
+
+  get '/tasks/:id' do
+    @task = Task.find(params[:id])
+    erb :show
+  end
+
+  get '/tasks/:id/edit' do
+    @task = Task.find(params[:id])
+    erb :edit
+  end
+
+
+  put '/tasks/:id' do |id|
+    Task.update(id.to_i, params[:task])
+    redirect "/tasks/#{id}"
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,11 +1,12 @@
 require 'sqlite3'
 
 class Task
-  attr_reader :title, :description
-  
+  attr_reader :title, :description, :id
+
   def initialize(task_params)
     @description = task_params["description"]
     @title       = task_params["title"]
+    @id = task_params["id"] if task_params["id"]
     @database    = SQLite3::Database.new('db/task_manager_development.db')
     @database.results_as_hash = true
   end
@@ -14,12 +15,32 @@ class Task
     @database.execute("INSERT INTO tasks (title, description) VALUES (?, ?);", @title, @description)
   end
 
-  def self.all
+  def self.database
     database = SQLite3::Database.new('db/task_manager_development.db')
     database.results_as_hash = true
+    database
+  end
+
+  def self.all
     tasks = database.execute("SELECT * FROM tasks")
     tasks.map do |task|
       Task.new(task)
     end
+  end
+
+  def self.find(id)
+    task = database.execute("SELECT * FROM tasks WHERE id = ?", id.to_i).first
+    Task.new(task)
+  end
+
+  def self.update(id, task_params)
+    database.execute("UPDATE Tasks
+                      SET title = ?,
+                          description = ?
+                      WHERE id = ?;",
+                      task_params[:title],
+                      task_params[:description],
+                      id)
+    Task.find(id)
   end
 end
